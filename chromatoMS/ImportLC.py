06f21fe70055ace1c9bd2498aadcd7c1f5f9810c
camille <camille@noemail.net>
2020-03-05 12:47:05 +0000

    add ImportLC.py code
    FossilOrigin-Name: d20a68f8f176ec1d04e218b24a3876275195abfaee726a50d2a75c0b571f73ea

---
+++ chromatoMS/ImportLC.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+"""
+program to import sets of LC-MS spectra
+processing is done on the fly
+It creates and returns a HDF5 file containing the data-set
+"""
+import sys
+
+from xml.etree import cElementTree as ET
+
+from spike.File.Solarix import *
+from spike.NPKData import TimeAxis
+from spike.File import HDF5File as hf
+from spike.util import progressbar as pg
+from spike.util import widgets
+from spike.NPKData import copyaxes
+
+
+def import_scan(filename):
+    """
+    Read scan.xml file and return the minutes, tic and max peak lists. Exit with error if scan.xml file doesn't exists.
+    """
+    if os.path.isfile(filename):
+        f = open(filename, "r")
+    else:
+        sys.exit("Cannot find scan.xml file in input folder")
+    data =f.read()
+    res = ET.fromstring(data)
+    tic_list = []
+    minutes_list = []
+    maxpeak_list = []
+    for word in res:
+        tic = float(word.find('tic').text)
+        tic_list.append(tic)
+        minutes = float(word.find('minutes').text)
+        minutes_list.append(minutes)
+        maxpeak = float(word.find('maxpeak').text)
+        maxpeak_list.append(maxpeak)    
+    f.close()
+    return (minutes_list, tic_list, maxpeak_list)
+
+def comp_sizes(si1, si2):
+    """
+    computes sizes of smaller version of dataset
+    """
+    allsz = []
+    while si1>1024 or si2>1024:
+        if si1>=2048:
+            si1 = si1//4
+        elif si1>=1024:
+            si1 = si1//2
+        if si2>= 8*1024:
+            si2 = si2//4
+        elif si2>=1024:
+            si2 = si2//2
+        allsz.append((si1,si2))
+    return allsz
+
+def Import_and_Process_LC(folder, outfile = "LC-MS.msh5", compress=False, downsample=True):
+    """
+    Entry point to import sets of LC-MS spectra
+    processing is done on the fly
+    It creates and returns a HDF5 file containing the data-set
+    
+    compression is active if (compress=True).
+    downsample is applied if (downsample=True).
+    These two parameters are efficient but it takes time.
+    """
+    parfilename = locate_acquisition(folder)
+    params = read_param(parfilename)
+    # get chromatogram
+    minu, tic, maxpk = import_scan( os.path.join(folder,"scan.xml") )   
+    # Import parameters : size in F1 and F2    
+    sizeF1 = len(minu)
+    sizeF2 = int(params["TD"])
+    if os.path.isfile(os.path.join(folder,"ser")):
+        fname = os.path.join(folder, "ser")
+    else:
+        raise Exception("You are dealing with 1D data, you should use Import_1D")
+    #size, specwidth,  offset, left_point, highmass, calibA, calibB, calibC, lowfreq, highfreq
+    data = FTICRData( dim=2 )   # create dummy LCMS
+    data.axis1 = TimeAxis(size=sizeF1, tabval=np.array(minu), importunit="min", currentunit='min' )    
+    data.axis2.size = 1*sizeF2    # The processing below might change the size, so we anticipate here !    
+    data.axis2.specwidth = float(params["SW_h"])   
+    found = False    # search for excitation bandwidth
+    try:
+        data.axis2.lowfreq, data.axis2.highfreq = read_ExciteSweep(locate_ExciteSweep(folder))
+        found = True
+    except:
+        pass
+    if not found:
+        try:
+            data.axis2.highfreq = float(params["EXC_Freq_High"])
+        except:
+            data.axis2.highfreq = data.axis2.calibA/float(params["EXC_low"])  # on Apex version
+        try:
+            data.axis2.lowfreq = float(params["EXC_Freq_Low"])
+        except:
+            data.axis2.lowfreq = data.axis2.calibA/float(params["EXC_hi"])  # on Apex version
+
+    data.axis2.highmass = float(params["MW_high"])
+    data.axis2.left_point = 0
+    data.axis2.offset = 0.0
+    data.axis2.calibA = float(params["ML1"])
+    data.axis2.calibB = float(params["ML2"])
+    data.axis2.calibC = float(params["ML3"])
+    if not math.isclose(data.axis2.calibC,0.0):
+        print('Using 3 parameters calibration,  Warning calibB is -ML2')
+        data.axis2.calibB *= -1
+
+    data.params = params   # add the parameters to the data-set
+    HF = hf.HDF5File(outfile,"w")
+    if compress:
+        HF.set_compression(True)
+    HF.create_from_template(data, group='resol1')
+    HF.store_internal_object(params, h5name='params')    # store params in the file
+    # then store files xx.methods and scan.xml
+    HF.store_internal_file(parfilename)
+    HF.store_internal_file( os.path.join(folder,"scan.xml") )
+    try:
+        HF.store_internal_file( locate_ExciteSweep(folder) )
+    except:
+        print('ExciteSweep file not stored')
+    data.hdf5file = HF
+    # I need a link back to the file in order to close it 
+    print(type(data.buffer))
+    print(data)
+    datalist = []   # remembers all downsampled dataset
+    if downsample:
+        allsizes = comp_sizes(data.size1, data.size2)
+        for i, (si1,si2) in enumerate(allsizes):
+            datai = FTICRData(dim = 2)
+            copyaxes(data,datai)
+            datai.axis1.size = si1
+            datai.axis2.size = si2
+            HF.create_from_template(datai, group='resol%d'%(i+2))
+            datalist.append(datai)           
+    if sys.maxsize  == 2**31-1:   # the flag used by array depends on architecture - here on 32bit
+        flag = 'l'              # Apex files are in int32
+    else:                       # here in 64bit
+        flag = 'i'              # strange, but works here.
+    spectre = FTICRData(shape=(sizeF2,))
+    Impwidgets = ['Importing: ', widgets.Percentage(), ' ', widgets.Bar(marker='-',left='[',right=']'), widgets.ETA()]
+    pbar = pg.ProgressBar(widgets=Impwidgets, maxval=sizeF1, fd=sys.stdout).start()
+    
+    with open(fname,"rb") as f:
+        ipacket = 0
+        szpacket = 10
+        packet = np.zeros((szpacket,sizeF2))   # store by packet to increase compression speed
+        for i1 in range(sizeF1):
+            #print(i1, ipacket, end='  ')
+            tbuf = f.read(4*sizeF2)
+            if len(tbuf) != 4*sizeF2:
+                break
+            abuf = np.array(array.array(flag,tbuf),dtype=float)
+            # processing
+            spectre.set_buffer(abuf)
+            spectre.adapt_size()
+            spectre.hamming().zf(2).rfft().modulus()  # double the size
+            mu, sigma = spectre.robust_stats(iterations=5)
+            spectre.buffer -= mu
+            spectre.zeroing(sigma*3).eroding()
+            packet[ipacket,:] = spectre.buffer[:]  # store into packet
+            if (ipacket+1)%szpacket == 0:          # and dump every szpacket
+                data.buffer[i1-(szpacket-1):i1+1,:] = packet[:,:]
+                packet[:,:] = 0.0
+                ipacket = 0
+            else:
+                ipacket += 1
+            # now downsample
+            for datai in datalist:
+                if i1%(sizeF1//datai.size1) == 0:   # modulo the size ratio
+                    ii1 = (i1*datai.size1) // sizeF1
+                    spectre.set_buffer(abuf)
+                    spectre.adapt_size()
+                    spectre.chsize(datai.size2).hamming().zf(2).rfft().modulus()
+                    mu, sigma = spectre.robust_stats(iterations=5)
+                    spectre.buffer -= mu
+                    spectre.zeroing(sigma*3).eroding()
+                    datai.buffer[ii1,:] = spectre.buffer[:]
+            pbar.update(i1)
+        # flush the remaining packet
+        data.buffer[i1-ipacket:i1,:] = packet[:ipacket,:]
+    pbar.finish()
+    HF.flush()
+    return data
+
+def main():
+    """
+    python ImportLC.py [options] importBrukfolder.d outputfile.msh5
+    options:
+    -c : do compression (True or False, default False)
+    -ds : perform downsampling (True or False, default False)
+    -e : erase previous output file if it already exists (True or False, default False: if file exists returns error)
+    """
+    import argparse
+    import time
+    import os.path
+
+    # Parse and interpret options.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('importBrukfolder', help='input folder that contains the sample to process')
+    parser.add_argument('outputfile', help='output file in msh5 format')
+    parser.add_argument('--doc', action='store_true', help="print a description of the program")
+    parser.add_argument('-c', '--compress', default=False, help="option to apply file compression, default is False")
+    parser.add_argument('-ds', '--downsampling', default=False, help="option to apply downsampling to the raw data to improve compression rate (but takes time), default is False")
+    parser.add_argument('-e', '--erase', default=False, help="creates a new output file if it already exists, default is False")
+    args = parser.parse_args()
+
+    if args.doc:
+        print(__doc__)
+        sys.exit(0)
+
+    infilename = args.importBrukfolder
+    outputfile = args.outputfile
+    compression = args.compress
+    downSampling = args.downsampling
+    erase = args.erase
+
+    if os.path.isfile(outputfile):
+        if erase == False:
+            sys.exit("File already exists, type a different output file name or erase the existing one")
+        else:
+            print("Output file will be rewritten")
+
+    t0 = time.time()
+    d = Import_and_Process_LC(infilename, outfile=outputfile, compress=compression, downsample=downSampling)
+    elaps = time.time()-t0
+    print('Processing took ',elaps/60," minutes")
+
+if __name__ == '__main__':
+    sys.exit(main())
